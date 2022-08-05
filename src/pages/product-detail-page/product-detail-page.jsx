548831f96234944ac74619b733ac60fdc56d45c7
nourdinesh <nsatoure@emeal.nttdata.com>
2022-08-05 13:34:56 +0200

    Added the selector with default value when there is only one option

--- src/pages/product-detail-page/product-detail-page.jsx
+++ src/pages/product-detail-page/product-detail-page.jsx
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from 'react-router-dom';
+import Select from 'react-select'
 import "./product-detail-page.scss"
 
 const ProductDetailPage = (props) => {
   const params = useParams();
   const [product, setProduct] = useState({});
+  const [storageOptions, setStorageOptions] = useState([]);
+  const [colorOptions, setColorOptions] = useState([]);
+
+  const handleSelectStorage = (value) => {
+    console.log("handleSelectStorage", value)
+  }
+
+  const handleSelectColor = (value) => {
+    console.log("handleSelectColor", value)
+  }
+
+  const formatToSelect = (array) => {
+    return array.map(storage =>{ return { value: storage.code, label: storage.name }});
+  }
 
   useEffect(() => {
     fetch(`https://front-test-api.herokuapp.com/api/product/${params.idDetail}`)
       .then(res => res.json())
       .then(
         (result) => {
-          setProduct(result)
+          setStorageOptions(formatToSelect(result.options.storages));
+          setColorOptions(formatToSelect(result.options.colors));
+          setProduct(result);
         },
         (error) => {
           console.log("error", error)
@@ -47,10 +64,21 @@ const ProductDetailPage = (props) => {
         </div>
         <div className="product-detail-page__info__actions">
           <div>Actions:</div>
-          <div>Selector title</div>
-          <div>Selector</div>
-          <div>Selector title</div>
-          <div>Selector</div>
+          <div>Storage</div>
+          {
+            storageOptions.length === 1 ?
+            <Select options={storageOptions} value={storageOptions[0]} onChange={handleSelectStorage} />
+            :
+            <Select options={storageOptions} onChange={handleSelectStorage} />
+          }
+         
+          <div>Color</div>
+          {
+            colorOptions.length === 1 ?
+            <Select options={colorOptions} value={colorOptions[0]} onChange={handleSelectColor} />
+            :
+           <Select options={colorOptions} onChange={handleSelectColor} />
+          }
           </div>
       </div>
     </div>
